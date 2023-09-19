2bb7090fe1230b3e32cbca5c8a207ed64ca53733
Maxime ROCHE <maxime.roche2023@campus-eni.fr>
2023-09-19 14:17:59 +0200

    Ajout Retrait Dao

---
+++ src/main/java/fr/eni/ecole/encheres/dal/RetraitDao.java
@@ -0,0 +1,22 @@
+
+package fr.eni.ecole.encheres.dal;
+
+import java.util.List;
+
+import fr.eni.ecole.encheres.bo.Retrait;
+
+public interface RetraitDao {
+
+	void save(Retrait retrait);
+
+	Retrait findOne(int idRetrait);
+
+	List<Retrait> findAll();
+
+	void modify(Retrait retrait);
+
+	void remove(int idRetrait);
+
+	List<Retrait> findByRetrait(String query);
+
+}
