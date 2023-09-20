dcec90d8d4fc7f4760c53dc101ceb2ddb9d2c46f
NymaRz <117542981+NymaRz@users.noreply.github.com>
2023-09-20 09:50:03 +0200

    Servlet et JSP, XML clean

---
+++ src/main/java/fr/eni/ecole/encheres/ihm/CreerCompteServlet.java
@@ -0,0 +1,23 @@
+package fr.eni.ecole.encheres.ihm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("")
+
+public class CreerCompteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
