329e87b3a5abbb58be11db4c67133bc3a4aef029
Sophie FOGELGESANG <sophie.fogelgesang2023@campus-eni.fr>
2023-09-21 14:29:16 +0200

    CHANGE VendreArticleServlet

--- src/main/java/fr/eni/ecole/encheres/ihm/VendreArticleServlet.java
+++ src/main/java/fr/eni/ecole/encheres/ihm/VendreArticleServlet.java
@@ -1,27 +1,67 @@
 package fr.eni.ecole.encheres.ihm;
 
+import java.io.IOException;
+import java.time.LocalDate;
+
+import fr.eni.ecole.encheres.bll.AdresseManager;
+import fr.eni.ecole.encheres.bll.CategorieManager;
+import fr.eni.ecole.encheres.bll.RetraitManager;
+import fr.eni.ecole.encheres.bo.Adresse;
+import fr.eni.ecole.encheres.bo.Categorie;
+import fr.eni.ecole.encheres.bo.Retrait;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-@WebServlet ("/vendre-article")
+@WebServlet("/vendre-article")
 
 public class VendreArticleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/pages/vendre-article.jsp");
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/pages/vendre-article.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String nomArticle = request.getParameter("nomArticle");
+			String description = request.getParameter("description");
+			Categorie categorie = CategorieManager.getInstance()
+					.recupUneCategorie(Integer.parseInt(request.getParameter("categorie")));
+			int miseAPrix = Integer.parseInt(request.getParameter("miseAPrix"));
+			LocalDate dateDebutEncheres;
+			if (request.getParameter("dateDebutEncheres") == null) {
+				dateDebutEncheres = LocalDate.now();
+			} else {
+				dateDebutEncheres = LocalDate.parse(request.getParameter("dateDebutEncheres"));
+			}
+			LocalDate dateFinEncheres = LocalDate.parse(request.getParameter("dateFinEncheres"));
+			if (dateFinEncheres.isBefore(dateDebutEncheres) || dateFinEncheres.isEqual(dateDebutEncheres)) {
+				throw new ServletException();
+			}
+
+			// prévoir de récupérer l'adresse de l'utilisateur par défaut
+			String rue = request.getParameter("rue");
+			String codePostal = request.getParameter("codePostal");
+			String ville = request.getParameter("ville");
+
+			Adresse adresse = new Adresse(rue,codePostal,ville);
+			AdresseManager.getInstance().ajouterUneAdresse(adresse);
+			Retrait retrait = new Retrait(adresse);
+			RetraitManager.getInstance().ajouterUnRetrait(retrait);
+			System.out.println(adresse.getIdAdresse());
+			
+
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
-
 
 //<h1>${ articleVendu.nomArticle }</h1>
 //
