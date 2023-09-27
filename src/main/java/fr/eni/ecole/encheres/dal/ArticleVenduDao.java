f058a9175d207e353f4c27b26d9b303d626c47d1
Sophie FOGELGESANG <sophie.fogelgesang2023@campus-eni.fr>
2023-09-27 16:30:20 +0200

    menu avancé si connecté.e + mise à jour des articles selon la date

--- src/main/java/fr/eni/ecole/encheres/dal/ArticleVenduDao.java
+++ src/main/java/fr/eni/ecole/encheres/dal/ArticleVenduDao.java
@@ -23,7 +23,30 @@ public interface ArticleVenduDao {
 
 	List<ArticleVendu> rechercheArticlesDeCategorieByName(int noCategorie, String query);
 
+
+
+	List<ArticleVendu> recupTousLesArticlesSelonEtatVente(String etatVente);
+
+	// retrouver les articles sur lequel l'utilisateur a enchéri
 	List<ArticleVendu> recupArticlesEncheresParUtilisateur(Utilisateur utilisateur);
 
+	List<ArticleVendu> recupArticlesEncherisParUtilisateurEtCategorie(Utilisateur utilisateur, int noCategorie);
+
+	List<ArticleVendu> recupUnArticleEncheriParutilisateurEtCategorie(Utilisateur utilisateur, int noCategorie,
+			String q);
+
+	List<ArticleVendu> recupUnArticleEncheriParUtilisateur(Utilisateur utilisateur, String q);
+
+	// fin retrouver les articles sur lequel l'utilisateur a enchéri
+//
+	// retrouver les ventes en cours de l'utilisateur
 	List<ArticleVendu> recupArticlesVendusParUtilisateurSelonEtatVente(Utilisateur utilisateur, String etatVente);
+	List<ArticleVendu> recupUnArticlesVendusParUtilisateurSelonEtatVenteEtCategorie(Utilisateur utilisateur,
+			int noCategorie, String q, String etatVente);
+
+	List<ArticleVendu> recupArticlesVendusParUtilisateurSelonEtatVenteEtCategorie(Utilisateur utilisateur,
+			int noCategorie, String etatVente);
+
+	List<ArticleVendu> recupUnArticlesVendusParUtilisateurSelonEtatVente(Utilisateur utilisateur, String q, String etatVente);
+	// fin retrouver les ventes en cours de l'utilisateur
 }
